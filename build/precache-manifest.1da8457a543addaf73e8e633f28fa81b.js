293fc69466b2cb3ddc38ecf0ea9dec4536bfd12c
Chris Warren <chris@chriswarren.dev>
2020-07-19 19:30:01 -0400

    Updated app to be ready to deploy on firebase

---
+++ build/precache-manifest.1da8457a543addaf73e8e633f28fa81b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f02fd8dee32b9b68b6432a9ac9f3c40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b6e70e00adf2ec44604",
+    "url": "/static/css/main.ebc1bf3a.chunk.css"
+  },
+  {
+    "revision": "cbad4ffa90308d6f97a7",
+    "url": "/static/js/2.59789b07.chunk.js"
+  },
+  {
+    "revision": "74c0ddca854ac003128884c6311253d0",
+    "url": "/static/js/2.59789b07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b6e70e00adf2ec44604",
+    "url": "/static/js/main.623ce3f6.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
